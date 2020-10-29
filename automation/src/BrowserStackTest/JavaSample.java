f4e1f2e5d292818a155bd0066b2f16e86e810fb6
Divyank Singh <divyanks@lambdatest.com>
2020-10-29 16:51:16 +0530

    Java automation

---
+++ automation/src/BrowserStackTest/JavaSample.java
@@ -0,0 +1,32 @@
+package BrowserStackTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
+
+public class JavaSample {
+    public static final String AUTOMATE_USERNAME = "divyanksingh1";
+    public static final String AUTOMATE_ACCESS_KEY = "CNnn3B4jqfsEFKbcJYyp";
+    public static final String URL = "https://" + AUTOMATE_USERNAME + ":" + AUTOMATE_ACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
+    public static void main(String[] args) throws Exception {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("os_version", "10");
+        caps.setCapability("resolution", "1920x1080");
+        caps.setCapability("browser", "Chrome");
+        caps.setCapability("browser_version", "latest");
+        caps.setCapability("os", "Windows");
+        caps.setCapability("name", "BStack-[Java] Sample Test"); // test name
+        caps.setCapability("build", "BStack Build Number 1"); // CI/CD job or build name
+        WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+        driver.get("https://www.google.com");
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("BrowserStack");
+        element.submit();
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+}
